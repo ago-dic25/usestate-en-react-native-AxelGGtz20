0634e8035e44c8bf48b247468a732ee962af99de
Axel Gabriel Gutierrez Ruano <axel.gutierrezr@uanl.edu.mx>
2025-10-28 08:25:59 -0600

    Actividad en clase

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 import { estiloTextos } from './misEstilos';
+import { useState } from 'react';
+import { StatusBar, TextInput } from 'react-native';
 
 export default function App() {
+  
+  const [contador, setContador] = useState(0);
+  const [nombre, setNombre] = useState("");
+  const [mensaje, setMensaje] = useState("");
+  
+  function saludarAlumno() {
+    if (nombre.trim() === "") {
+      setMensaje("Escribe tu nombre...");
+    } else {
+      setMensaje("Hola " + nombre);
+    }
+  }
+
+  function contadorCaracteres() {
+    setMensaje("Tu nombre tiene " + nombre.length + " caracteres");
+  }
+  function emojiPorLongitud() {
+    if (nombre.length === 0) {
+      setMensaje("🎃");
+    } else if (nombre.length > 1 && nombre.length < 10) {
+      setMensaje("👻👻👻👻👻");
+    } else {
+      setMensaje("🧛🧛🧛🧛🧛🧛🧛🧛🧛🧛🧛🧛");
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={estiloTextos.texto}>Hola, mundo</Text>
+      <Text>{mensaje}</Text>
+      <TextInput
+        placeholder='Escribe tu nombre'
+        value = {nombre}
+        onChangeText = {setNombre}
+      />
+      <Button title = "Mostrar Mensaje" onPress={saludarAlumno}></Button>
+      <Button title = "Contar caracteres" onPress={contadorCaracteres}></Button>
+      <Button title = "Emoji por longitud" onPress={emojiPorLongitud}></Button>
+      <Button title = "Limpiar" onPress={()=> {setNombre(""); setMensaje(""); }}></Button>
+      <Text>Haz hecho clic {contador} veces </Text>
+      <Button title = "Aumentar" onPress={()=> setContador(contador + 1)}></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +52,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   }
